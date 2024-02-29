8d00427c0ed6b2d62c3296ce5cebf11dfb49da6c
Mohamed Hamdy <opgghamdy@gmailcom>
2024-02-29 12:55:34 +0200

    upload files

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NgForOf } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgForOf],
+  imports: [NgForOf, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
